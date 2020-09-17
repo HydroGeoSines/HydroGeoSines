de951a20a4e93a821b2d2073c0b0f82c5a9fff9b
Gabriel Rau <32972360+hydrogeoscience@users.noreply.github.com>
2020-09-17 11:12:29 +0200

    Update test_brf_v0.py

--- test_brf_v0.py
+++ test_brf_v0.py
@@ -10,10 +10,10 @@ hgs = hgs.site('TEST', geoloc=[141.762065, -31.065781, 160])
 fg_data = hgs.data.import_csv('test_data/fowlers_gap/acworth_short.csv', dt_fmt='%d/%m/%Y %H:%M')
 print(fg_data)
 
-timefloat = hgs.data.tf.values
-data = hgs.data.all['{BP}'].values
-data1, result1 = hgs.model.lin_window_ovrlp(timefloat, data)
-data2, result2 = hgs.model.hals(timefloat, data1, freqs='AT')
+# timefloat = hgs.data.tf.values
+# data = hgs.data.all['{BP}'].values
+# data1, result1 = hgs.model.lin_window_ovrlp(timefloat, data)
+# data2, result2 = hgs.model.hals(timefloat, data1, freqs='AT')
 
 #%% 
 heads = hgs.data.correct_heads(locs=['Smith'])
