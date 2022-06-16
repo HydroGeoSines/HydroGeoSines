ed9ed926c20e1992cb665f1e574ef4c13e1a1cb7
Chris Turnadge <chris.turnadge@csiro.au>
2022-06-16 13:46:25 +0930

    Create front_page.py

---
+++ apps/front_page.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pygtide
+import streamlit as st
+
+st.subheader("Workflows")
+st.write("\n1. Data preprocessing [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
+st.write("\n2. Barometric compensation [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
+st.write("\n3. Earth tide estimation [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
+st.write("\n4. Barometric response function [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
